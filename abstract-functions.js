c3143b1bb6b4e27963aaffd319cb9f8ccba26b77
Thies Bucker <thiesbert@yahoo.de>
2021-03-09 15:45:48 +0100

    created function

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,13 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+function ç(name, height, favoriteColor){
+    console.log(`My name is ${name}, and my height is ${height}, and my favorite color is ${favoriteColor}`);
+}
 
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
 
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+personalIntroduction("Bob", "72", "red");
+personalIntroduction("Samantha", "65", "orange");
+personalIntroduction("Christopher", "69", "green");
+personalIntroduction("Tamera", "63", "blue");
 
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
